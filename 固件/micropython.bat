e4d7557d2f7df71bc679106c7b1b960ea22e4611
woodcol <gp@woodcol.com>
2025-05-19 17:02:29 +0800

    增加主控板之间通过无线通信息的功能

--- 固件/micropython.bat
+++ 固件/micropython.bat
@@ -3,7 +3,7 @@ pip install esptool==3.3 -i https://pypi.tuna.tsinghua.edu.cn/simple
 cd /d %~dp0
 
 esptool.py -b 115200  erase_flash
-esptool.py --chip esp8266 --baud 115200 write_flash --flash_size=detect 0 "1_22_2-20250511.bin"
+esptool.py --chip esp8266 --baud 115200 write_flash --flash_size=detect 0 "1_22_2-20250519.bin"
 
 esptool.py run
 
