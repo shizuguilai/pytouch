6783d68dbf5adabda9b35166d9be2d13ce500195
woodcol <gp@woodcol.com>
2025-06-10 11:20:41 +0800

    修改上传批处理文件

--- 固件/micropython.bat
+++ 固件/micropython.bat
@@ -2,9 +2,9 @@ pip install esptool==3.3 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 cd /d %~dp0
 
-esptool.py -b 115200  erase_flash
-esptool.py --chip esp8266 --baud 115200 write_flash --flash_size=detect 0 "1_22_2-20250519.bin"
+esptool -b 115200  erase_flash || esptool.py -b 115200  erase_flash
+esptool --chip esp8266 --baud 115200 write_flash --flash_size=detect 0 1_22_2-20250519.bin || esptool.py --chip esp8266 --baud 115200 write_flash --flash_size=detect 0 1_22_2-20250519.bin
 
-esptool.py run
+esptool run || esptool.py run
 
 pause
