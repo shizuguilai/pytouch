7e4018f7e1ca82d544243a9333561bd0ef34021b
woodcol <gp@woodcol.com>
2025-06-28 18:45:06 +0800

    增加了巴法云控制16路继电器的程序

--- src/巴法云/主控板py继电器程序/main.py
+++ src/巴法云/主控板py继电器程序/main.py
@@ -74,34 +74,75 @@ def is_int(dat):
     except:
         return False
 
+MSGTYPE = 0       #消息类型,
+KEY_Delay = 10    #继电器延时时间,单位秒
+#0表示1~16号为继电器关闭,1表示1~16号为继电器打开,2表示1~16号为继电器延时打开,3表示1~16号为继电器延时关闭
+
+#16个按键的延时时间
+delayKes = {1:-1,2:-1,3:-1,4:-1,5:-1,6:-1,7:-1,8:-1,9:-1,10:-1,11:-1,12:-1,13:-1,14:-1,15:-1,16:-1}
+keyStates = {1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1,12:1,13:1,14:1,15:1,16:1} #0表示继电器关闭,1表示继电器打开
+
+def keyChange(p):
+    if MSGTYPE == 0:
+        unTouchPin(p)
+        keyStates[p] = 1
+        delayKes[p] = -1
+    elif MSGTYPE == 1:
+        touchPin(p)
+        keyStates[p] = 0
+        delayKes[p] = -1
+    elif MSGTYPE == 2:
+        delayKes[p] = KEY_Delay
+        unTouchPin(p)
+        keyStates[p] = 1
+    elif MSGTYPE == 3:
+        delayKes[p] = KEY_Delay
+        touchPin(p)
+        keyStates[p] = 0
+
 #巴法云回调函数,收到巴法云发来的消息后会被调用
 def bfCallback(dat):
+    global MSGTYPE
     print(dat)
     intdat = -1
     if is_int(dat): #收到按钮1~按钮16的点击指令,对应点击头点击一次
         intdat = int(dat)
-    if dat == 'h':                      #所有点击头击一次
-        setAllPinStates(0)              #16个点击头都按下
-        time.sleep_ms(80)               #按下时间80毫秒
-        setAllPinStates(allUntouch)     #16个点击头都抬起
-    elif intdat >=1 and intdat <= 16:   #对次编号的点击头点击一次
-        touchOncePin(intdat)
-    elif dat == 'a':           #向上滑动,j1,j2,j3,j4
-        moveUP()
-    elif dat == 'b':           #向下滑动,j1,j2,j3,j4
-        moveDown()
-    elif dat == 'c':           #快速双击
-        doubleTouchFast(4)      #快速双击J4
-    elif dat == 'd':           #长按
-        longTouch(4)            #快速双击J4
-    elif dat == 'e':           #慢速双击,用来点亮屏幕
-        doubleTouch(4)         #慢速双击J4
+    if intdat >=1 and intdat <= 16:   #对次编号的点击头点击一次
+        keyChange(intdat)
+    elif dat == 'a':           #0表示1~16号为继电器关闭
+        MSGTYPE = 0
+    elif dat == 'b':           #1表示1~16号为继电器打开
+        MSGTYPE = 1
+    elif dat == 'c':           #2表示1~16号为继电器延时打开
+        MSGTYPE = 2
+    elif dat == 'd':           #表示1~16号为继电器延时关闭
+        MSGTYPE = 3
+    elif dat == 'e':           #所有继电器打开
+        setAllPinStates(0xffff)
+        for i in range(1,17):
+            keyStates[i] = 1
+            delayKes[i] = -1
+    elif dat == 'f':           #所有继电器关闭
+        setAllPinStates(0x0000) 
+        for i in range(1,17):
+            keyStates[i] = 0
+            delayKes[i] = -1
+    elif dat == 'g':           #所有继电器延时打开一段时间
+        setAllPinStates(0xffff)
+        for i in range(1,17):
+            keyStates[i] = 1
+            delayKes[i] = KEY_Delay
+    elif dat == 'h':           #所有继电器延时关闭一段时间
+        setAllPinStates(0x0000)
+        for i in range(1,17):
+            keyStates[i] = 0
+            delayKes[i] = KEY_Delay
     else:
         print('unknow command:%s'%(dat)) #你们发的命令,自行解析吧
 
 #主函数,点击器程序从这里开始运行
 def main():
-    setAllPinStates(allUntouch)               #初始化所有点击头为抬起状态
+    setAllPinStates(0)               #初始化所有点击头为抬起状态
     uid = '14712ccdbc88a59817d77c64ca2e4c33'  #巴法云uid,自行替换
     ssid = 'woodcol24'                        #wifi名称,自行替换
     password = '7668150Ww00'                #wifi密码,自行替换
@@ -109,6 +150,17 @@ def main():
     BFUtil.start(uid,bfCallback)              #设置巴法云的uid,并设置接收消息回调函数,同时启动巴法云
     while True:
         time.sleep(1)                         #这里是死循环,延时1秒,可以写别的逻辑
+        for i in range(1,17):                  #循环16次,判断16个点击头是否延时打开或关闭
+            if delayKes[i] >= 0:
+                delayKes[i] -= 1
+                if delayKes[i] < 0:
+                    if keyStates[i] == 1:
+                        touchPin(i)
+                        keyStates[i] = 0
+                    elif keyStates[i] == 0:
+                        unTouchPin(i)
+                        keyStates[i] = 1
+                        
 
 if __name__ == '__main__':  
     main()
