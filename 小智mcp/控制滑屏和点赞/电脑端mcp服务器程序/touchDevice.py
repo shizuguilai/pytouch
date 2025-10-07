5129267e769d4743e507c0390240e0d56b05b2d3
woodcol <gp@woodcol.com>
2025-10-07 18:10:33 +0800

    no message

--- 小智mcp/控制滑屏和点赞/电脑端mcp服务器程序/touchDevice.py
+++ 小智mcp/控制滑屏和点赞/电脑端mcp服务器程序/touchDevice.py
@@ -19,11 +19,11 @@ mcp = FastMCP("touchDevice")
 
 touchUtil.openSerial()
 
-cmddict = {1:'x',2:'y',3:'1',4:'2',5:'z'}
+cmddict = {1:'x',2:'y',3:'z',4:'1',5:'2'}
 # Add an addition tool
 @mcp.tool()
 def touchDevice(cmd: int) -> dict:
-    """当要求滑动屏幕,或者操作手机时,使用这个工具来操作手机,cmd为操作指令,支持下边指令:\n1:向上滑动或者跳过广告\n2:向下滑动\n3:关闭广告\n4:返回上一个界面\n5:点赞或双击屏幕\n"""
+    """当要求滑动屏幕,或者操作手机时,使用这个工具来操作手机,cmd为操作指令,支持下边指令:\n1:向上滑动或者跳过广告\n2:向下滑动\n3:点赞或双击屏幕\n4:返回上一个界面\n5:关闭广告\n"""
     print('Touch device,cmd:',cmd)
     result = touchUtil.sendcmd(cmddict[cmd])
     logger.info(f"touchDevice formula: {cmd}, result: {result}")
