2b4af3762d85078374fa3f5c3dc19c2df7bc5bfb
woodcol <gp@woodcol.com>
2025-12-15 17:56:54 +0800

    增加光感应传感器

---
+++ src/ADC传感器/main.py
@@ -0,0 +1,90 @@
+#power by fengmm521.taobao.com
+#wechat:woodmage
+from machine import ADC, Pin
+import tDriver as t
+import time
+
+
+# 初始化ADC（ESP8266只有一个ADC引脚，连接到TOUT引脚）
+adc = ADC(0)  # 创建ADC对象，参数0表示ADC引脚
+
+#实始化一个点击器控制实例对象
+tobj = t.TouchObj()
+#生成随机整数
+def randint(min, max):
+    return t.randint(min, max)
+#pNumber按键按下,pNumber从1~16
+def touchPin(pNumber):
+    tobj.setPin(pNumber,False)
+    tobj.updateData()
+#pNumber按键抬起,pNumber从1~16
+def unTouchPin(pNumber):
+    tobj.setPin(pNumber,True)
+    tobj.updateData()
+#按下并抬起,即点按一次,pNumber从1~16
+def touchOncePin(pNumber):
+    #判断输入的点击头编号是否在1~16范围内,不在时程序直接返回,并报错
+    if pNumber < 1 or pNumber > 16: 
+        print('pin number erro:%d'%(pNumber)) 
+        return
+    touchPin(pNumber)   #对应点击头按下
+    time.sleep_ms(80)   #按下时间为80毫秒,这个时间可以根据需要修改,建议30~100之间
+    unTouchPin(pNumber) #对应点击头抬起
+    time.sleep_ms(80)   #抬起后再等80毫秒
+
+
+#使用四个点击头组成一列进行向上滑动操作, 从下向上依次是J1,J2,J3,J4的方式排列
+def moveUP():
+    tobj.move([1,2,3,4])  #按J1,J2,J3,J4顺序滑动
+#向下滑动操作,正好和向上相反
+def moveDown():
+    tobj.move([4,3,2,1]) #按J4,J3,J2,J1顺序滑动
+
+
+allUntouch = 0b1111111111111111    #所有点击头抬起状态
+danshu     = 0b1010101010101010    #单数按下状态
+shuanshu   = 0b0101010101010101    #双数按下状态
+
+#使用16位的按键状态数一次控制所有点击头状态
+def setAllPinStates(state):
+    tobj.set16Pins(state)
+    tobj.updateData()
+
+def touchAll():
+    setAllPinStates(0)
+    time.sleep_ms(50)
+    setAllPinStates(allUntouch)
+    time.sleep_ms(50)
+
+isRUN = False                      #定义程序运行控制全局变量
+
+def checkADC():
+    """读取原始ADC值并转换为电压"""
+    # 读取原始值（0-1023）
+    raw_value = adc.read()
+    print(raw_value)
+    if raw_value < 300:
+        return True
+    else:
+        return False
+
+#主函数,点击器程序从这里开始运行
+def main():
+    global isRUN                     #引用全局变量isRUN
+    setAllPinStates(allUntouch)      #初始化所有点击头为抬起状态
+    while True:
+        tkey = tobj.key.value()      #检测物理按键是否被按下,当按键按下时tkey为0,否则为1
+        if not tkey:
+            isRUN = not isRUN        #程序运行状态取反
+            time.sleep(1)            #按键被按下,按键消抖延时1秒
+        if isRUN:
+            if checkADC():               #程序运行时检测ADC值
+                touchAll()
+        else:
+            time.sleep_ms(100)       #程序未运行时延时100毫秒
+
+if __name__ == '__main__':  
+    main()
+
+
+
