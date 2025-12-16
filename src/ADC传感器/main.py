b7a60677f6847ed2c6a9776ad74110781636fa4f
woodcol <gp@woodcol.com>
2025-12-16 16:11:28 +0800

    修改ADC参数设置

--- src/ADC传感器/main.py
+++ src/ADC传感器/main.py
@@ -3,6 +3,7 @@
 from machine import ADC, Pin
 import tDriver as t
 import time
+import uartUtil
 
 # 初始化ADC（ESP8266只有一个ADC引脚，连接到TOUT引脚）
 adc = ADC(0)  # 创建ADC对象，参数0表示ADC引脚
@@ -49,20 +50,79 @@ def setAllPinStates(state):
     tobj.set16Pins(state)
     tobj.updateData()
 
-def touchAll():
-    setAllPinStates(0)
-    time.sleep_ms(50)
+def touchDanShu(dtu = 30,dtd = 50):
+    setAllPinStates(danshu)
+    time.sleep_ms(dtd)
     setAllPinStates(allUntouch)
-    time.sleep_ms(50)
+    time.sleep_ms(dtu)
 
+def touchShuangShu(dtu = 30,dtd = 50):
+    setAllPinStates(shuanshu)
+    time.sleep_ms(dtd)
+    setAllPinStates(allUntouch)
+    time.sleep_ms(dtu)
+
+isSetData = False #是否多触点操作
+mutildat = ''
+
+adcConfig = 300
+
+isShowADC = True
+
+
+
+def setADCData():
+    global adcConfig
+    if len(mutildat) > 0:
+        try:
+            adcConfig = int(mutildat.strip())
+            print('adc config is:',adcConfig)
+            return
+        except Exception:
+            adcConfig = 300
+            print('set adc config fomart error')
+    else:
+        print('set adc config data empty')
+    with open('config.txt','w') as f:
+        f.write(str(adcConfig))
+def getADCData():
+    global adcConfig
+    try:
+        with open('config.txt','r') as f:
+            adcConfig = int(f.read().strip())
+            print('adc config is:',adcConfig)
+    except Exception:
+        adcConfig = 300
+        print('not config.txt file,set default:300')
+def uartCheck():
+    global isShowADC
+    dat = uartUtil.reciveDat(True) #True表示每次只接收一个字节数据,只要有数据就一直接收,默认是False,接收一行数据,只有以换行符结束才会返回
+    if dat: #接收到数据,dat将不为None
+        print('recive uart data:%s'%(dat.decode())) #decode是把字节流数据转成字符串格式
+        dat = dat.decode().strip() #strip()是去掉字符串两边的空格
+        print(len(dat),dat)
+        if dat == '[':
+            isSetData = True
+        elif dat == ']':
+            isSetData = False
+            setADCData()
+        elif isSetData and (dat >= '0' and dat <= '9'):
+            mutildat += dat
+        elif dat == '0':
+            isShowADC = False
+        elif dat == '1':
+            isShowADC = True
+        else:
+            print('error data:%s'%(dat))
 isRUN = False                      #定义程序运行控制全局变量
 
 def checkADC():
     """读取原始ADC值并转换为电压"""
     # 读取原始值（0-1023）
     raw_value = adc.read()
-    print(raw_value)
-    if raw_value < 300:
+    if isShowADC:
+        print(raw_value)
+    if raw_value < adcConfig:
         return True
     else:
         return False
@@ -71,16 +131,20 @@ def checkADC():
 def main():
     global isRUN                     #引用全局变量isRUN
     setAllPinStates(allUntouch)      #初始化所有点击头为抬起状态
+    getADCData()
     while True:
         tkey = tobj.key.value()      #检测物理按键是否被按下,当按键按下时tkey为0,否则为1
         if not tkey:
             isRUN = not isRUN        #程序运行状态取反
             time.sleep(1)            #按键被按下,按键消抖延时1秒
         if isRUN:
-            if checkADC():               #程序运行时检测ADC值
-                touchAll()
+            if checkADC():               #程序运行时检测ADC值,界面出现时,ADC值小于300
+                touchDanShu()
+            else:
+                touchShuangShu()         #程序未运行时,ADC值大于300
         else:
             time.sleep_ms(100)       #程序未运行时延时100毫秒
+            uartCheck()                  #检查串口是否有数据输入,在这个函数中进行接收
 
 if __name__ == '__main__':  
     main()
