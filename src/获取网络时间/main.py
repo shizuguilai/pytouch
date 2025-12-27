a3762c9a656f30dd3ca2d7288bbac4dcbb04a555
woodcol <gp@woodcol.com>
2025-12-27 18:00:36 +0800

    增加获取网络时间的RTC例子

--- src/获取网络时间/main.py
+++ src/获取网络时间/main.py
@@ -1,6 +1,32 @@
+#power by fengmm521.taobao.com
+#wechat:woodmage
+import machine
+import ujson
+#文件是否存在
+def isExists(pth):
+    try:
+        f = open(pth,'rb')
+        f.close()
+        return True
+    except Exception:
+        return False
+SSID = None
+PASSWORD = None
+if not isExists('wifi.json'):
+    import webconfig
+    webconfig.WebConfig().run() #开始web配网,配网成功后自动保存wifi.json,然后重新启动
+else:
+    with open('wifi.json', 'r') as f:
+        config = ujson.load(f)
+        SSID = config.get('ssid')
+        PASSWORD = config.get('password')
+        print('wifi config:',SSID,PASSWORD)
+
 from machine import RTC
 import time
 import ntptime
+
+
 
 #这个工具,当设备连上网络后,可以获取网络时间,并设置到RTC
 rtc = RTC() 
@@ -95,3 +121,16 @@ class LibTime:
     # 当前线程睡眠
     def sleep_ms(self, ms):
         time.sleep_ms(ms)
+
+def test():
+    import socketUtil
+    socketUtil.connect_wifi(SSID,PASSWORD)  #让板子连接wifi
+    tobj = LibTime()
+    tobj.syncRemote() #同步网络时间,同时打印北京时间
+    dftime = tobj.getTime() #获取默认UTC时间
+    print("dftime:",dftime)
+    bjtime = tobj.getBjTime() #获取北京时间
+    print("bjtime:",bjtime)
+    #这样获取到时间后,就可以结合出厂程序里的控制点击头的逻辑来用程序控制和时间有关的定时点击操作了
+if __name__ == '__main__':
+    test()
